02df03eb3b0f1e88f84f18614273d623ca73b8bf
TuDouZa1 <1715593110@qq.com>
2026-01-25 22:08:07 +0800

    feat(blog): 新增vim快捷键笔记文章和info样式

--- js/js.js
+++ js/js.js
@@ -1,4 +1,20 @@
+// 获取html文件名
+function showCurrentFileName() {
+  // 提取文件名
+  const urlPaths = window.location.href.split("/");
+  const fileName = urlPaths[urlPaths.length - 1].split("?")[0];
+  // 解码中文
+  const decodedFileName = decodeURIComponent(fileName).replace(".html", "");
+  // 显示文件名
+  document.getElementById("fileName").textContent = decodedFileName;
+}
+
 document.querySelectorAll("pre code").forEach((block) => {
   block.textContent = block.innerHTML;
 });
-hljs.highlightAll();
+
+document.addEventListener("DOMContentLoaded", function () {
+  // 在页面加载完成后执行
+  hljs.highlightAll();
+  showCurrentFileName();
+});
